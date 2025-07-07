8f5baad02a3301ed140b6baef9030ff54b09f316
Sugandh04 <sugandh.ishaan@gmail.com>
2025-07-07 09:32:33 +0530

    Added play again option

--- number_game.java
+++ number_game.java
@@ -3,30 +3,40 @@ import java.util.Scanner;
 
 public class number_game {
     public static void main(String[] args) {
-        
-        Random random = new Random();
-         int low =1;
-         int high = 100;
-        int randomNumber = random.nextInt(high - low + 1) + low;
 
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Welcome to the Number Guessing Game!");
-        System.out.println("Choose a number between 1 and 100.");
+        Random random = new Random();
+        int low = 1;
+        int high = 100;
+
+        Scanner sc = new Scanner(System.in);
         int attempts = 1;
         int guess;
-        do{
+        int game = 1;
+        while (game == 1) {
+            System.out.println("Welcome to the Number Guessing Game!");
+            System.out.println("Choose a number between 1 and 100.");
+            int randomNumber = random.nextInt(high - low + 1) + low;
+            do{
             System.out.print("Enter your guess: ");
             guess = sc.nextInt();
-            if (guess < randomNumber){
+            if (guess < randomNumber) {
                 System.out.println("Warmer");
-            attempts++;
-            }
-            else if (guess > randomNumber){
+                attempts++;
+            } else if (guess > randomNumber) {
                 System.out.println("Colder");
                 attempts++;
             }
-        }
+        }       
         while (guess != randomNumber);
-        System.out.println("Congratulations! You've guessed the number " + randomNumber + " in " + attempts + " attempts.");
+
+            System.out.println( "Congratulations! You've guessed the number " + randomNumber + " in " + attempts + " attempts.");
+            System.out.println("Press 1 to play again");
+            game = sc.nextInt();
+            if(game!=1)
+            {
+                System.out.println("Thank you for playing!");
+                break;
+            }
         }
     }
+}
