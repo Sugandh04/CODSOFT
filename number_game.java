d25aea6541f9c05d2ed0631937a197297743d20c
Sugandh04 <sugandh.ishaan@gmail.com>
2025-07-07 12:31:16 +0530

    Limited number of attempts

--- number_game.java
+++ number_game.java
@@ -12,29 +12,42 @@ public class number_game {
         int attempts = 1;
         int guess;
         int game = 1;
+        int max_attempts=10;
         while (game == 1) {
+            attempts = 1;
             System.out.println("Welcome to the Number Guessing Game!");
+            System.out.println("You have 10 attempts at max to guess the number");        
             System.out.println("Choose a number between 1 and 100.");
             int randomNumber = random.nextInt(high - low + 1) + low;
-            do{
-            System.out.print("Enter your guess: ");
-            guess = sc.nextInt();
-            if (guess < randomNumber) {
-                System.out.println("Warmer");
-                attempts++;
-            } else if (guess > randomNumber) {
-                System.out.println("Colder");
-                attempts++;
-            }
-        }       
-        while (guess != randomNumber);
+            do {
+                System.out.print("Enter your guess: ");
+                guess = sc.nextInt();
+                if (guess < randomNumber) {
+                    System.out.println("Warmer");
+                    attempts++;
+                    max_attempts--;
+                } else if (guess > randomNumber) {
+                    System.out.println("Colder");
+                    attempts++;
+                    max_attempts--;
+                }
+                if(max_attempts==1){
+                    break;
+                }
+                
+            } while (guess != randomNumber);
 
-            System.out.println( "Congratulations! You've guessed the number " + randomNumber + " in " + attempts + " attempts.");
-            attempts=1;
+            if(max_attempts<=1){
+                System.out.println("Out of chances");
+            }
+            else {
+            System.out.println("Congratulations! You've guessed the number " + randomNumber + " in " + attempts + " attempts.");
+            }
+
+
             System.out.println("Press 1 to play again");
             game = sc.nextInt();
-            if(game!=1)
-            {
+            if (game != 1) {
                 System.out.println("Thank you for playing!");
                 break;
             }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+            // if(max_attempts <=1){
+            //     System.out.println("No attempts lefts.");
+            //     System.out.println("The number was: " + randomNumber);
+            // }
